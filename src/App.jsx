5a968562025e2bf3f6838a4b975bf40ddd08ca0e
Jayaharish2004 <115623388+Jayaharish2004@users.noreply.github.com>
2024-07-23 16:07:11 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+
+function App() {
+  return(
+    <div>
+      {/* <Login/> */}
+      <Register/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/button/Button.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Button() {
+    <Button variant="contained">Contained</Button>
+}
+export default Button;
